9516fc1d62639ca482f35d83bcd032c5f365c268
Jon Wilson <jon.wilson0082@gmail.com>
2025-09-09 12:49:16 -0500

    last one

--- terraform/main.tf
+++ terraform/main.tf
@@ -273,7 +273,6 @@ resource "aws_lambda_function" "api" {
   environment {
     variables = {
       BLOGS_TABLE_NAME      = aws_dynamodb_table.blogs.name
-      TEMPLATES_TABLE_NAME  = aws_dynamodb_table.templates.name
       TEMPLATES_BUCKET_NAME = aws_s3_bucket.templates.bucket
     }
   }
